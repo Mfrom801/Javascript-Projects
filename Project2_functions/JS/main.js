fd3a4073b1b4305afe8f61b9c5098b18903bfbb1
Mfrom801 <mfrom801@gmail.com>
2023-01-04 15:10:09 -0700

    Updated project 2

--- Project2_functions/JS/main.js
+++ Project2_functions/JS/main.js
@@ -1,4 +1,10 @@
-function My_First_Function() {
+function My_First_Function() {  // This is the first function
     var str = "This is the button text!";
     document.getElementById("Button_Text").innerHTML = str;
+}
+
+function myFunction() {     // This is the second function for concatenating two strings
+    var sentence = "I am learning";
+    sentence += " a lot from this book!";
+    document.getElementById("Concatenate").innerHTML = sentence;
 }
