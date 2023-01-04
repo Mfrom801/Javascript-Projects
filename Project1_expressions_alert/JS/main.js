28a26f8d3d089198ec0b953a93e056f9494d5d24
Mfrom801 <mfrom801@gmail.com>
2023-01-04 14:42:07 -0700

    Updated Javascript projects

---
+++ Project1_expressions_alert/JS/main.js
@@ -0,0 +1,8 @@
+var x="Woah, bro.";  // giving x the value of a string
+var GuitarFamily = "Electric", Dad= "Gibson", Mom= "Fender", Son= "Jackson"; // Stating a variable
+window.alert(x);
+document.write(x);
+document.write("He told that lady" + " Food is good."); // adding two strings together
+document.write(Mom);
+
+document.write(11+1);
