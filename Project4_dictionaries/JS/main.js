4f6b056da7ca13bb030b1a588d5d4450db463f4c
Mfrom801 <mfrom801@gmail.com>
2023-01-04 18:13:27 -0700

    Update

--- Project4_dictionaries/JS/main.js
+++ Project4_dictionaries/JS/main.js
@@ -6,5 +6,6 @@ function my_Dictionary()  {
         MFD: "12-22-2017",
         Age: 5,
     };
-    document.getElementById("Dictionary").innerHTML = Guitar.Color;
+    delete Guitar.Color
+    document.getElementById("Dictionary").innerHTML = Guitar.Color;  // where value is removed from the dictionary before it can be processed.
 }
---
+++ Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,6 @@
+var z="</br>";
+var x="10";
+var y="5";
+document.write(typeof 9);
+document.write(z)
+document.write(y + x);
