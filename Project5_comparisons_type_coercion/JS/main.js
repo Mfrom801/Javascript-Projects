34001da36a0ca6eb70ac08ce379dfedce4a71a8e
Mfrom801 <mfrom801@gmail.com>
2023-01-06 17:53:12 -0700

    Updated project 5

--- Project5_comparisons_type_coercion/JS/main.js
+++ Project5_comparisons_type_coercion/JS/main.js
@@ -3,7 +3,7 @@ var x="10";
 var e="10";
 var d="Ten";
 var y="5";
-document.write(typeof 9);
+document.write(typeof 9);     // Here are a list of Comparisons and operations
 document.write(z)
 document.write(y + x);
 
@@ -17,7 +17,7 @@ document.write(z);
 document.write(11>15);
 document.write(z);
 console.log(2+5);
-document.write(z);
+document.write(z);      //Boolean comparisons
 console.log(15>19);
 document.write(15==15);
 document.write(z);
@@ -37,15 +37,15 @@ document.write(z);
 
 document.write(8>7 && 10>2);
 document.write(z);
-document.write(6>7 && 10>7);
+document.write(7<6 && 10>7);
 document.write(z);
 document.write(11>7 || 10>8);
 document.write(z);
-document.write(9>15 || 10>25);
+document.write(9>15 || 25<10);
 document.write(z);
 
 document.write(z);
-
+// not functions
 function not_function()  {
     document.getElementById("Not").innerHTML = !(20>10);
 }
