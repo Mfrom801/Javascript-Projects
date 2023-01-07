14c7383f4cdc357588d7afb89f576ce78ca7a2e2
Mfrom801 <mfrom801@gmail.com>
2023-01-06 17:19:31 -0700

    Updated comparisons projects

--- Project5_comparisons_type_coercion/JS/main.js
+++ Project5_comparisons_type_coercion/JS/main.js
@@ -1,6 +1,55 @@
 var z="</br>";
 var x="10";
+var e="10";
+var d="Ten";
 var y="5";
 document.write(typeof 9);
 document.write(z)
 document.write(y + x);
+
+document.write(z);
+document.write(2E310);
+document.write(z);
+document.write(-3E310);
+document.write(z);
+document.write(10>5);
+document.write(z);
+document.write(11>15);
+document.write(z);
+console.log(2+5);
+document.write(z);
+console.log(15>19);
+document.write(15==15);
+document.write(z);
+document.write(6==15);
+document.write(z);
+document.write(x===e);
+
+document.write(z);
+document.write(z===y);
+document.write(z);
+document.write(x===d);
+document.write(z);
+document.write(x===y);
+document.write(z);
+
+
+
+document.write(8>7 && 10>2);
+document.write(z);
+document.write(6>7 && 10>7);
+document.write(z);
+document.write(11>7 || 10>8);
+document.write(z);
+document.write(9>15 || 10>25);
+document.write(z);
+
+document.write(z);
+
+function not_function()  {
+    document.getElementById("Not").innerHTML = !(20>10);
+}
+
+function not_function1()  {
+    document.getElementById("Not").innerHTML = !(5>10);
+}
